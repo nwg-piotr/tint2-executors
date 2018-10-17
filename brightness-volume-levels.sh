acf8bc505b132c54166201510447122cd0971949
piotr <nwg.piotr@gmail.com>
2018-10-18 01:03:27 +0200

    brightness and volume levels

---
+++ brightness-volume-levels.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Dependencies: `alsa-utils`, `xorg-xbacklight` or `light-git`
+
+# Use the line below with `xorg-xbacklight`
+bri=B:$(xbacklight -get | cut -d'.' -f1)%
+
+# or replace with the line below for `light-git` (https://github.com/haikarainen/light, should work on Radeon graphics)
+# bri=B:$(light -G | cut -d'.' -f1)%
+
+vol=V:$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')
+
+out=$bri"\n"$vol
+echo -e $out
