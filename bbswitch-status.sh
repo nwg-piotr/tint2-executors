652fb0f96db8f9a2432a7964ac0169560b12068b
piotr <nwg.piotr@gmail.com>
2018-10-18 01:55:37 +0200

    description added

--- bbswitch-status.sh
+++ bbswitch-status.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Credits: dsboger/gnome-shell-extension-bumblebee-status
+# https://github.com/dsboger/gnome-shell-extension-bumblebee-status
+
 bb_status=$(cat /proc/acpi/bbswitch | awk -F ' ' '{print $2}')
 
 if [ "$bb_status" = "ON" ]; then
