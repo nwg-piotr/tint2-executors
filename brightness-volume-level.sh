2ea2ddd13a5987cc56ef8189c8a0fd6998c550e1
piotr <nwg.piotr@gmail.com>
2018-10-20 00:13:15 +0200

    Bug fixed: file should be detected instead of directory

--- brightness-volume-level.sh
+++ brightness-volume-level.sh
@@ -6,17 +6,18 @@
 # Project: https://github.com/nwg-piotr/tint2-executors
 # License: GPL3
 
-# Prefer the `light` package, use `xbacklight` if `light` not found
-if [[ $(which light) == *"/light"* ]]
-then
-    b=$(light -G)
+# Credits: dsboger/gnome-shell-extension-bumblebee-status
+# https://github.com/dsboger/gnome-shell-extension-bumblebee-status
+# no-bumblebee icon by @edskeye
+
+if [ -f "/proc/acpi/bbswitch" ]; then
+
+    bb_status=$(cat /proc/acpi/bbswitch | awk -F ' ' '{print $2}')
+    if [ "$bb_status" = "ON" ]; then
+        echo ~/tint2-executors/images/nvidia.svg
+    elif [ "$bb_status" = "OFF" ]; then
+        echo ~/tint2-executors/images/intel.svg
+    fi
 else
-    b=$(xbacklight -get)
+    echo ~/tint2-executors/images/no-bumblebee.svg
 fi
-
-# Lets round the float result
-bri=B:$(echo "($b+0.5)/1" | bc)%
-
-vol=V:$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')
-
-echo -e ${bri}"\n"${vol}
