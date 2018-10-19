689f256fcc78524bad4dccc7ba49a65f6c063a63
piotr <nwg.piotr@gmail.com>
2018-10-19 03:24:05 +0200

    brightness rounding improved; light preferred before xbacklight

--- brightness-volume-level.sh
+++ brightness-volume-level.sh
@@ -6,13 +6,17 @@
 # Project: https://github.com/nwg-piotr/tint2-executors
 # License: GPL3
 
-# Use the line below with `xorg-xbacklight`
-bri=B:$(xbacklight -get | cut -d'.' -f1)%
+# Prefer the `light` package, use `xbacklight` if `light` not found
+if [[ $(which light) == *"/light"* ]]
+then
+    b=$(light -G)
+else
+    b=$(xbacklight -get)
+fi
 
-# or replace with the line below to use the `light` package (should work with Radeon graphics)
-# bri=B:$(light -G | cut -d'.' -f1)%
+# Lets round the float result
+bri=B:$(echo "($b+0.5)/1" | bc)%
 
 vol=V:$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')
 
-out=$bri"\n"$vol
-echo -e $out
+echo -e ${bri}"\n"${vol}
