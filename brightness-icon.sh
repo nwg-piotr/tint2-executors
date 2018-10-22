174183a681bd30caabd11339bffeb93c9387578a
piotr <nwg.piotr@gmail.com>
2018-10-23 00:54:17 +0200

    Brightness icon added

---
+++ brightness-icon.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script display an appropriate brightness icon according to the brightness level
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Dependencies: `xbacklight` or `light-git'; `obhud` (AUR) for left/right click executor actions
+
+# Prefer the `light` package, use `xbacklight` if `light` not found
+if [[ $(which light) == *"/light"* ]]
+then
+    b=$(light -G)
+else
+    b=$(xbacklight -get)
+fi
+
+# Lets round the float result
+bri=$(echo "($b+0.5)/1" | bc)
+
+if [ "$bri" -gt "90" ]; then
+    echo ~/tint2-executors/images/bri-full.svg
+elif [ "$bri" -gt "60" ]; then
+    echo ~/tint2-executors/images/bri-high.svg
+elif [ "$bri" -gt "40" ]; then
+    echo ~/tint2-executors/images/bri-medium.svg
+else
+    echo ~/tint2-executors/images/bri-low.svg
+fi
