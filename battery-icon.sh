80a1271679a9fc3b3654ee571df6788292416390
nwg-piotr <nwg.piotr@gmail.com>
2018-10-24 15:58:02 +0200

    Battery icon added

---
+++ battery-icon.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# This script displays battery icons according to the load level and charging state
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Dependencies: `acpi`
+
+bat=$(acpi -b)
+state=$(echo $bat | awk '{print $3}')
+level=$(echo $bat | awk '{print $4}')
+level=${level::-2}
+
+if [ "$state" = "Charging," ]; then
+
+    if [ "$level" -ge "99" ]; then
+        echo ~/tint2-executors/images/bat-full-charging.svg
+    elif [ "$level" -ge "50" ]; then
+        echo ~/tint2-executors/images/bat-medium-charging.svg
+    elif [ "$level" -ge "30" ]; then
+        echo ~/tint2-executors/images/bat-low-charging.svg
+    else
+        echo ~/tint2-executors/images/bat-empty-charging.svg
+    fi
+else
+    if [ "$level" -ge "99" ]; then
+        echo ~/tint2-executors/images/bat-full.svg
+    elif [ "$level" -ge "50" ]; then
+        echo ~/tint2-executors/images/bat-medium.svg
+    elif [ "$level" -ge "30" ]; then
+        echo ~/tint2-executors/images/bat-low.svg
+    else
+        echo ~/tint2-executors/images/bat-empty.svg
+    fi
+fi
