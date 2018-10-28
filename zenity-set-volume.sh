10dd02aee4f5b4c8da0dd80e049b874d01f50d44
piotr <nwg.piotr@gmail.com>
2018-10-28 13:43:21 +0100

    just added

---
+++ zenity-set-volume.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Set the volume level with a zenity dialog box
+
+# Dependencies: `alsa-utils`, `zenity`
+
+lvl=$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')
+lvl=${lvl::-1}
+lvl=$(zenity --scale --value ${lvl} --title "Volume" --text "Set master volume level")
+amixer sset 'Master' ${lvl}% -q
