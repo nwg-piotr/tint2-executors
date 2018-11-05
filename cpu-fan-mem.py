e3fa354e1c76ce7b32f91e223774b2345220bcd2
piotr <nwg.piotr@gmail.com>
2018-11-05 23:54:58 +0100

    Ready for testing

--- cpu-fan-mem.py
+++ cpu-fan-mem.py
@@ -47,7 +47,6 @@ import time
 
 
 def main():
-
     fahrenheit = False
     testing = False
     time_start = None
@@ -69,7 +68,7 @@ def main():
     if testing:
         time_start = int(round(time.time() * 1000))
 
-    pcpu, avg, speed, temp = None, None, None, None
+    pcpu, avg, speed, temp, fans, memory = None, None, None, None, None, None
     output = ""
 
     # prepare requested data only once
@@ -87,6 +86,15 @@ def main():
 
     if "t" in components:
         temp = psutil.sensors_temperatures(fahrenheit)
+
+    if "f" in components:
+        try:
+            fans = psutil.sensors_fans()
+        except:
+            pass
+
+    if "m" in components or "M" in components:
+        memory = psutil.virtual_memory()
 
     for char in components:
         if char == "g":
@@ -109,84 +117,22 @@ def main():
             output += " " + str(temp["coretemp"][0][1])
             output += "℉ " if fahrenheit else "℃ "
 
+        if char == "f" and fans is not None and len(fans) > 0:
+            fan0 = next(iter(fans.values()))
+            output += " " + str(fan0[0][1]) + "/m "
+
+        if char == 'm' and memory is not None:
+            output += " " + str(round((memory[0] - memory[1]) / 1073741824, 1)) + "GB "
+
+        if char == 'M' and memory is not None:
+            output += " " + str(round((memory[0] - memory[1]) / 1073741824, 1)) + "/" + str(
+                round(memory[0] / 1073741824, 1)) + "GB "
+
     # remove double spaces and print
     print(re.sub(' +', ' ', output))
 
     if testing:
         print("\nIt took " + str(int((round(time.time() * 1000)) - time_start) / 1000) + " s")
-
-    exit(0)
-
-    display_graph = True
-    display_average = False
-    display_percentage = False
-    short = False
-
-    if len(sys.argv) > 1 and sys.argv[1].startswith("-"):
-        options = sys.argv[1][1:].upper()
-        short = options.find("S") > -1
-        display_average = options.find("A") > -1
-        display_percentage = options.find("P") > -1
-        display_graph = options.find("G") > -1
-        fahrenheit = options.find("F") > -1
-
-    if not display_average and not display_percentage:
-        display_graph = True
-
-    # CPU load
-    if display_graph or display_percentage:
-
-        result = psutil.cpu_percent(interval=1, percpu=True)
-
-        if display_graph:
-            # Load per CPU as semi-graph:
-            print(graph_per_cpu(result), end=" ")
-
-        if display_percentage:
-            # Load per CPU as percentage:
-            print("CPU%: " + per_cpu(result), end = "")
-
-    if display_average:
-        # CPU average load:
-        print("CPU: " + str(psutil.cpu_percent(interval=1)), end = "% ")
-
-    # CPU frequency
-    try:
-        freq = psutil.cpu_freq(False)
-        if freq is not None:
-            print(str(round(freq[0] / 1000, 1)), end="")
-            if not short:
-                print("/" + str(round(freq[2] / 1000, 1)), end="")
-            print("GHz", end=" ")
-    except:
-        pass
-
-    # Temperature sensor
-    try:
-        temp = psutil.sensors_temperatures(fahrenheit)      # True for Fahrenheit
-        if len(temp) > 0:                                   # Temperature sensor found!
-            core_temp = temp["coretemp"]                    # "acpitz" for ACPI Thermal Zone
-            print(core_temp[0][1], end="℉" if fahrenheit else "℃")
-    except:
-        pass
-
-    # Fan speed
-    try:
-        fans = psutil.sensors_fans()
-        if len(fans) > 0:                                   # Fan sensor found!
-            fan0 = next(iter(fans.values()))
-            print(" " + str(fan0[0][1]), end="/m ")
-        else:
-            print(" ", end = "")
-    except:
-        pass
-
-    # Memory: used/total
-    memory = psutil.virtual_memory()
-    print(str(round((memory[0] - memory[1]) / 1073741824, 1)), end="")
-    if not short:
-        print("/" + str(round(memory[0] / 1073741824, 1)), end="")
-    print("GB ")
 
 
 def per_cpu(result):
@@ -200,7 +146,6 @@ def per_cpu(result):
 
 
 def graph_per_cpu(result):
-
     graph = "_▁▂▃▄▅▆▇███"
 
     string = ""
