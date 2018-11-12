f978b629aec168817cd214314e9e368e3f2d3644
piotr <nwg.piotr@gmail.com>
2018-11-12 22:14:14 +0100

    Syntax improved; bat-half icon till 35%.

--- battery-icon.sh
+++ battery-icon.sh
@@ -11,14 +11,14 @@
 # Dependencies: `acpi`
 
 bat=$(acpi -b)
-state=$(echo $bat | awk '{print $3}')
+state=$(echo ${bat} | awk '{print $3}')
 
-if [ "$state" = "Not" ]; then
-    level=$(echo $bat | awk '{print $5}')
+if [[ "$state" = "Not" ]]; then
+    level=$(echo ${bat} | awk '{print $5}')
     level=${level::-1}
 
 else
-    level=$(echo $bat | awk '{print $4}')
+    level=$(echo ${bat} | awk '{print $4}')
     if [[ "$state" == *"Unknown"* ]]; then
         level=${level::-1}
     else
@@ -28,25 +28,25 @@ fi
 
 if [[ "$bat" == *"until"* ]]; then
 
-    if [ "$level" -ge "95" ]; then
+    if [[ "$level" -ge "95" ]]; then
         echo ~/tint2-executors/images/bat-full-charging.svg
-    elif [ "$level" -ge "75" ]; then
+    elif [[ "$level" -ge "75" ]]; then
         echo ~/tint2-executors/images/bat-threefourth-charging.svg
-    elif [ "$level" -ge "40" ]; then
+    elif [[ "$level" -ge "35" ]]; then
         echo ~/tint2-executors/images/bat-half-charging.svg
-    elif [ "$level" -ge "15" ]; then
+    elif [[ "$level" -ge "15" ]]; then
         echo ~/tint2-executors/images/bat-quarter-charging.svg
     else
         echo ~/tint2-executors/images/bat-empty-charging.svg
     fi
 else
-    if [ "$level" -ge "95" ]; then
+    if [[ "$level" -ge "95" ]]; then
         echo ~/tint2-executors/images/bat-full.svg
-    elif [ "$level" -ge "75" ]; then
+    elif [[ "$level" -ge "75" ]]; then
         echo ~/tint2-executors/images/bat-threefourth.svg
-    elif [ "$level" -ge "40" ]; then
+    elif [[ "$level" -ge "35" ]]; then
         echo ~/tint2-executors/images/bat-half.svg
-    elif [ "$level" -ge "15" ]; then
+    elif [[ "$level" -ge "15" ]]; then
         echo ~/tint2-executors/images/bat-quarter.svg
     else
         echo ~/tint2-executors/images/bat-empty.svg
