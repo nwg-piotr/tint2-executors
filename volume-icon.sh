49717f482d0281d3a636b7753311bb49cc629cf3
piotr <nwg.piotr@gmail.com>
2018-11-15 01:33:10 +0100

    -i argument for optional volume level (text)

--- volume-icon.sh
+++ volume-icon.sh
@@ -10,21 +10,24 @@
 
 # Dependencies: `alsa-utils`, `libnotify`, `xfce4-notifyd` or another notification server
 
-if [ "$(amixer sget Master | awk -F'[][]' '/Right:|Mono:/ && NF > 1 {print $4}')" = "on" ]; then
+if [[ "$(amixer sget Master | awk -F'[][]' '/Right:|Mono:/ && NF > 1 {print $4}')" = "on" ]]; then
 
     # search for the lines containing 'Right:' or 'Mono:', when more than 1 field exists
     # we strip the trailing '%' and round it up with printf "%0.0f" just in case
     vol=$(amixer sget Master | awk -F'[][]' '/Right:|Mono:/ && NF > 1 {sub(/%/, ""); printf "%0.0f\n", $2}')
 
-    if [ $vol -ge 90 ]; then
+    if [[ ${vol} -ge 90 ]]; then
         echo ~/tint2-executors/images/vol-full.svg
-    elif [ $vol -ge 40 ]; then
+    elif [[ ${vol} -ge 40 ]]; then
         echo ~/tint2-executors/images/vol-medium.svg
-    elif [ $vol -ge 10 ]; then
+    elif [[ ${vol} -ge 10 ]]; then
         echo ~/tint2-executors/images/vol-low.svg
     else
         echo ~/tint2-executors/images/vol-lowest.svg
     fi
 else
     echo ~/tint2-executors/images/vol-muted.svg
+fi
+if  [[ $1 = "-i" ]]; then
+    echo ${vol}%
 fi
