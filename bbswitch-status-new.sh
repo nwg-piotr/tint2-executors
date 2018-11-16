d63759ec5f053e4bfaae309f7278048dc3b2e33f
piotr <nwg.piotr@gmail.com>
2018-11-16 01:28:21 +0100

    icon + text

---
+++ bbswitch-status-new.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Credits: dsboger/gnome-shell-extension-bumblebee-status
+# https://github.com/dsboger/gnome-shell-extension-bumblebee-status
+# no-bumblebee icon by @edskeye
+
+if [[ -f "/proc/acpi/bbswitch" ]]; then
+
+    bb_status=$(cat /proc/acpi/bbswitch | awk -F ' ' '{print $2}')
+    if [[ "$bb_status" = "ON" ]]; then
+
+        t=$(nvidia-smi -q -d TEMPERATURE | grep "GPU Current Temp" | awk -F ' ' '{ print $5 }')
+
+        echo ~/tint2-executors/images/nvidia.svg;
+        echo ${t}"℃"
+
+    elif [[ "$bb_status" = "OFF" ]]; then
+        echo ~/tint2-executors/images/intel.svg
+    fi
+else
+    echo ~/tint2-executors/images/no-bumblebee.svg
+fi
