fd1f145d89e7792788bb01a22c131b7fb8414a04
piotr <nwg.piotr@gmail.com>
2018-11-19 11:51:35 +0100

    Displays current wifi name

---
+++ wifi-name.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Icon by @edskeye
+
+name="No Wi-Fi"
+
+wifi=$(iwgetid | awk -F '"' '{ print $2 }')
+if [[ ! -z "$wifi" ]]; then
+    name="$wifi"
+fi
+
+if [[ $1 == -N* ]]; then
+    echo "Wi-Fi: $name"
+elif [[ $1 == -M* ]]; then
+    echo "$(echo $1 | cut -c3-) $name"
+else
+    echo ~/tint2-executors/images/network.svg
+    echo ${name}
+fi
+
