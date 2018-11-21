0eacc30ff8c0d72ac826784dd9ed4f5a52c72696
piotr <nwg.piotr@gmail.com>
2018-11-21 02:38:11 +0100

    preparation to AUR

---
+++ arch-package/t2ec-lib/desktop.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+
+This script uses `wmctrl` to switch desktops, or just prints path to the icon corresponding to currently active desktop,
+followed by the desktop number as text.
+
+Author: Piotr Miller
+e-mail: nwg.piotr@gmail.com
+Website: http://nwg.pl
+Project: https://github.com/nwg-piotr/tint2-executors
+License: GPL3
+
+Command: python ~/tint2-executors/desktop.py [-n] | [-p] | [number]
+
+[-n] - switch to next desktop
+[-p] - switch to previous desktop
+[number] - switch to desktop of number given
+
+"""
+
+import sys
+import subprocess
+from pathlib import Path
+
+
+def main():
+    output = subprocess.check_output("wmctrl -d", shell=True)
+    desktops = output.splitlines()
+
+    current = current_desktop(desktops)
+    last = len(desktops) - 1
+
+    if len(sys.argv) > 1:
+        if sys.argv[1].upper() == "N" or sys.argv[1].upper() == "-N":
+            next_desktop(current, last)
+        elif sys.argv[1].upper() == "P" or sys.argv[1].upper() == "-P":
+            previous_desktop(current, last)
+        else:
+            try:
+                d = int(sys.argv[1])
+                select_desktop(d - 1, last)
+            except ValueError:
+                print("Argument not allowed. Should be: `desktop.py [-n] | [-p] | [number]`")
+    else:
+        print(str("/usr/share/t2ec/desktop.svg"))
+        print(str(current_desktop(desktops) + 1))
+
+
+def current_desktop(desktops):
+    for d in range(len(desktops)):
+        if str(desktops[d]).find("*") > -1:
+            return d
+
+
+def next_desktop(current, last):
+    n = current + 1 if current + 1 <= last else 0
+    subprocess.call(["wmctrl", "-s", str(n)])
+
+
+def previous_desktop(current, last):
+    n = current - 1 if current - 1 >= 0 else last
+    subprocess.call(["wmctrl", "-s", str(n)])
+
+
+def select_desktop(which, last):
+    if 0 <= which <= last:
+        subprocess.call(["wmctrl", "-s", str(which)])
+    else:
+        print("You only have desktops 1-" + str(last + 1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ arch-package/t2ec-lib/volume-icon.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# This script display an appropriate volume icon according to the volume level
+
+# Authors: Piotr Miller, @natemaia
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Dependencies: `alsa-utils`
+
+if [[ "$(amixer sget Master | awk -F'[][]' '/Right:|Mono:/ && NF > 1 {print $4}')" = "on" ]]; then
+
+    # search for the lines containing 'Right:' or 'Mono:', when more than 1 field exists
+    # we strip the trailing '%' and round it up with printf "%0.0f" just in case
+    vol=$(amixer sget Master | awk -F'[][]' '/Right:|Mono:/ && NF > 1 {sub(/%/, ""); printf "%0.0f\n", $2}')
+
+    if [[ ${vol} -ge 90 ]]; then
+        echo /usr/share/t2ec/vol-full.svg
+    elif [[ ${vol} -ge 40 ]]; then
+        echo /usr/share/t2ec/vol-medium.svg
+    elif [[ ${vol} -ge 10 ]]; then
+        echo /usr/share/t2ec/vol-low.svg
+    else
+        echo /usr/share/t2ec/vol-lowest.svg
+    fi
+    echo ${vol}%
+else
+    echo /usr/share/t2ec/vol-muted.svg
+fi
+if [[ $1 == up ]]; then
+    exec amixer set Master 5%+ unmute -q
+elif [[ $1 == down ]]; then
+    exec amixer set Master 5%- -q
+elif [[ $1 == toggle ]]; then
+    exec amixer set Master toggle -q
+fi
