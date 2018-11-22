1a41a90a4e10552e921b7965a7c8efea34b89fd6
piotr <nwg.piotr@gmail.com>
2018-11-22 14:24:24 +0100

    Accept `-N` argument do display "Bri: " + <level> instead of the icon

--- arch-package/t2ec-lib/battery-icon.sh
+++ arch-package/t2ec-lib/battery-icon.sh
@@ -12,51 +12,53 @@
 # argument: [-l] - append textual brightness level
 
 bat=$(acpi -b)
-state=$(echo ${bat} | awk '{print $3}')
+if [[ $bat ]]; then
+    state=$(echo ${bat} | awk '{print $3}')
 
-if [[ "$state" = "Not" ]]; then
-    level=$(echo ${bat} | awk '{print $5}')
-    level=${level::-1}
-
-else
-    level=$(echo ${bat} | awk '{print $4}')
-    if [[ "$state" == *"Unknown"* ]]; then
+    if [[ "$state" = "Not" ]]; then
+        level=$(echo ${bat} | awk '{print $5}')
         level=${level::-1}
-    else
-        level=${level::-2}
-    fi
-fi
 
-if [[ $1 == -N* ]]; then
-        echo "Bat: ${level}%"
-else
-    if [[ "$bat" == *"until"* ]]; then
-
-        if [[ "$level" -ge "95" ]]; then
-            echo /usr/share/t2ec/bat-full-charging.svg
-        elif [[ "$level" -ge "75" ]]; then
-            echo /usr/share/t2ec/bat-threefourth-charging.svg
-        elif [[ "$level" -ge "35" ]]; then
-            echo /usr/share/t2ec/bat-half-charging.svg
-        elif [[ "$level" -ge "15" ]]; then
-            echo /usr/share/t2ec/bat-quarter-charging.svg
-        else
-            echo /usr/share/t2ec/bat-empty-charging.svg
-        fi
     else
-        if [[ "$level" -ge "95" ]]; then
-            echo /usr/share/t2ec/images/bat-full.svg
-        elif [[ "$level" -ge "75" ]]; then
-            echo /usr/share/t2ec/bat-threefourth.svg
-        elif [[ "$level" -ge "35" ]]; then
-            echo /usr/share/t2ec/bat-half.svg
-        elif [[ "$level" -ge "15" ]]; then
-            echo /usr/share/t2ec/bat-quarter.svg
+        level=$(echo ${bat} | awk '{print $4}')
+        if [[ "$state" == *"Unknown"* ]]; then
+            level=${level::-1}
         else
-            echo /usr/share/t2ec/bat-empty.svg
+            level=${level::-2}
         fi
     fi
-    if  [[ $1 = "-l" ]]; then
-        echo ${level}%
+
+    if [[ $1 == -N* ]]; then
+            echo "Bat: ${level}%"
+    else
+        if [[ "$bat" == *"until"* ]]; then
+
+            if [[ "$level" -ge "95" ]]; then
+                echo /usr/share/t2ec/bat-full-charging.svg
+            elif [[ "$level" -ge "75" ]]; then
+                echo /usr/share/t2ec/bat-threefourth-charging.svg
+            elif [[ "$level" -ge "35" ]]; then
+                echo /usr/share/t2ec/bat-half-charging.svg
+            elif [[ "$level" -ge "15" ]]; then
+                echo /usr/share/t2ec/bat-quarter-charging.svg
+            else
+                echo /usr/share/t2ec/bat-empty-charging.svg
+            fi
+        else
+            if [[ "$level" -ge "95" ]]; then
+                echo /usr/share/t2ec/images/bat-full.svg
+            elif [[ "$level" -ge "75" ]]; then
+                echo /usr/share/t2ec/bat-threefourth.svg
+            elif [[ "$level" -ge "35" ]]; then
+                echo /usr/share/t2ec/bat-half.svg
+            elif [[ "$level" -ge "15" ]]; then
+                echo /usr/share/t2ec/bat-quarter.svg
+            else
+                echo /usr/share/t2ec/bat-empty.svg
+            fi
+        fi
+        if  [[ $1 = "-l" ]]; then
+            echo ${level}%
+        fi
     fi
 fi
