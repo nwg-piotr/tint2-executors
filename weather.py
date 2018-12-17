2be28a5ace93cdc684b23939ceb7bb18812556bd
piotr <nwg.piotr@gmail.com>
2018-12-17 02:02:39 +0100

    openweathermap.org weather

--- weather.py
+++ weather.py
@@ -7,37 +7,36 @@ Development just started. Nothing useful inside yet.
 Dependencies: wget
 Items:
     S - Short description (main)
-    D - Description
+    D - Description (description)
     T - Temp [℃ | ℉]
     P - Pressure [hpa]
     H - Humidity [%]
     W - Wind [m/s, degrees]
-    C - City name
+    C - City name (name)
 """
 
 import subprocess
 import json
 from collections import namedtuple
 import locale
+import os
 
 
 def main():
     response = None
-    # Defaults
-    api_key = ""
-    city_id = "2643743"  # London, UK
-    units = "metric"
-    lang = "en"
+    settings = Settings()
 
-    # todo These values will later be read from preferences
-    api_key = "f060ab40f2b012e72350f6acc413132a"
-    city_id = "7532606"
-    units = "metric"
+    if settings.lang is None:
+        loc = locale.getdefaultlocale()[0][:2]
+        settings.lang = loc if loc else "en"
 
-    lang = locale.getdefaultlocale()[0][:2]
+    print(settings.api_key)
+    print(settings.city_id)
+    print(settings.units)
+    print(settings.lang)
 
-    request_url = "http://api.openweathermap.org/data/2.5/weather?id=" + city_id + "&appid=" + api_key + \
-                  "&units=" + units + "&lang=" +lang
+    request_url = "http://api.openweathermap.org/data/2.5/weather?id=" + settings.city_id + "&appid=" + \
+                  settings.api_key + "&units=" + settings.units + "&lang=" + settings.lang
     try:
         response = subprocess.check_output("wget -qO- '" + request_url + "'", shell=True)
     except subprocess.CalledProcessError as exitcode:
@@ -52,9 +51,52 @@ def main():
             print(owm)
             print(owm.weather)
             print(getattr(owm.weather[0], "main"))
+            print(getattr(owm.main, "temp"))
 
         else:
             print("Error accessing openweathermap.org, HTTP status: " + str(owm.cod))
+
+
+class Settings:
+    def __init__(self):
+        super().__init__()
+
+        t2ec_dir = os.getenv("HOME") + "/.t2ecol"
+        # Create settings file if not found
+        if not os.path.isdir(t2ec_dir):
+            os.makedirs(t2ec_dir)
+        if not os.path.isfile(t2ec_dir + "/weatherrc"):
+            config = [
+                "# Items: [S]hort description, [D]escription, [T]emperature, [P]ressure, [H]umidity, [W]ind, [C]ity name\n",
+                "# API key: go to http://openweathermap.org and get one\n"
+                "# units may be metric or imperial\n"
+                "# uncomment lang to override system $LANG value\n"
+                "# ------------------------------------------------\n"
+                "items = CSTW\n",
+                "api_key = your_key_here\n",
+                "city_id = 2643743\n",
+                "units = metric\n",
+                "#lang = en"]
+
+            subprocess.call(["echo '" + ''.join(config) + "' > " + t2ec_dir + "/weatherrc"], shell=True)
+
+        self.api_key = ""
+        self.city_id = "2643743"  # London, UK
+        self.units = "metric"
+        self.lang = None
+
+        # read settings file
+        lines = open(t2ec_dir + "/weatherrc", 'r').read().rstrip().splitlines()
+        for line in lines:
+            if not line.startswith("#"):
+                if line.startswith("api_key"):
+                    self.api_key = line.split("=")[1].strip()
+                elif line.startswith("city_id"):
+                    self.city_id = line.split("=")[1].strip()
+                elif line.startswith("units"):
+                    self.units = line.split("=")[1].strip()
+                elif line.startswith("lang"):
+                    self.lang = line.split("=")[1].strip()
 
 
 if __name__ == "__main__":
