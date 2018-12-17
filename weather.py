2a886f5a9eb888876eea00fd20b3a2207475453f
nwg-piotr <nwg.piotr@gmail.com>
2018-12-17 14:55:32 +0100

    openweathermap weather (in progress)

--- weather.py
+++ weather.py
@@ -69,6 +69,25 @@ def main():
 
 
 def print_output(owm):
+    icons = {'01d': '01d.png',
+             '01n': '01n.png',
+             '02d': '02d.png',
+             '02n': '02n.png',
+             '03d': '03d.png',
+             '03n': '03d.png',
+             '04d': '04d.png',
+             '04n': '04d.png',
+             '09d': '09d.png',
+             '09n': '09d.png',
+             '10d': '10d.png',
+             '10n': '10n.png',
+             '11d': '11d.png',
+             '11n': '11d.png',
+             '13d': '13d.png',
+             '13n': '13d.png',
+             '50d': '50d.png',
+             '50n': '50d.png'}
+
     if owm.cod == 200:
         print(owm)
         print(owm.weather)
